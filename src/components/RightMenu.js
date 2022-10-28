653dfb321d439f0802a0bfc0c34ccd258942f0e8
lizasilent <elizavetad@yandex.ru>
2022-10-28 13:46:20 +0300

    right column details

--- src/components/RightMenu.js
+++ src/components/RightMenu.js
@@ -3,7 +3,6 @@ import React from "react";
 function RightMenu() {
   return (
     <div className="right">
-
       <div className="user">
         <div className="user__info">
           <p className="user__name">Lizaveta Silentova</p>
@@ -12,12 +11,24 @@ function RightMenu() {
         <div className="user__logo"></div>
       </div>
 
-      <div className="music">
-
-        
+      <div className="music"></div>
+      <div className="time">
+        <div className="weather"></div>
       </div>
-      <div></div>
-      <div></div>
+
+
+      <div className="ads-info">
+    <div className="close-icon"></div>
+    <div> 
+        <p className="ads-info__header">Unsleash the freelance super power</p>
+        <p  className="ads-info__text">Unlimited task, premium features and much more.</p>
+        </div>
+       
+        <div className="ads-info__images">
+            <div className="ads-info__img-left"></div>
+            <button className="ads-info__button"></button>
+        </div>
+      </div>
     </div>
   );
 }
