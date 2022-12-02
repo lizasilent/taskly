34ac4f0b96d534c4597ec5d02eb6434460680ee8
lizasilent <elizavetad@yandex.ru>
2022-12-02 17:14:45 +0200

    calendar

--- src/components/LeftMenu.js
+++ src/components/LeftMenu.js
@@ -1,5 +1,9 @@
 import React from "react";
 import logo from "../images/Logo2.png";
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+
+
 
 
 function LeftMenu() {
@@ -17,9 +21,29 @@ function LeftMenu() {
                 <p className="weekly__header-small">View all</p>
             </div>
 
+            <div className="weekly__list-block">
+
+                <ul className="weekly__list">
+                    <li  className="weekly__item">
+                        <div>
+                        <img src={logo} alt=""></img>
+                        <p></p>
+                        <p></p>
+                        </div>
+                       
+                       <div></div>
+
+                    </li>
+                </ul>
+
+</div>
+<Calendar />
 
         </div>
-        <div className="calendar"></div>
+
+
+
+   
        </div>
 
     )
