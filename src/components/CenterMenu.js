9b7197e19f680da558668cda0ee76326b2b712d2
lizasilent <elizavetad@yandex.ru>
2022-12-06 18:32:41 +0200

    Revert "mid header"
    This reverts commit aada384e57c305bb12fe339a006a3439373f0801.

--- src/components/CenterMenu.js
+++ src/components/CenterMenu.js
@@ -1,61 +1,44 @@
 import React from "react";
-import arrowback from "../images/back.png";
-import arrownext from "../images/front.png";
-import add from "../images/plus.png";
 
 function CenterMenu() {
-  return (
-    <div className="middle">
-      <div className="middle__header">
 
-        <div>
+
+
+    return (
+       <div className="middle">
+
+        <div className="middle__header">
         <p className="text">Today’s schedule</p>
-
-        <div className="">
-          <p className="text yellow">Friday 28 oct</p>
-          <div className ="arrow-block">
-            <img src={arrowback} alt="back"></img>
-            <img src={arrownext} alt="next"></img>
-          </div>
+        <p className="text yellow">Friday 28 oct</p>
         </div>
 
-        </div>
-        
-        <div>
-          <img src={add} alt="plus"></img>
-        </div>
-
-
-       
-      </div>
-
-      <div className="middle__list-block">
+        <div className="middle__list-block">
         <ul className="middle__list">
-          <li className="middle__item">
-            <div className="middle__icon">⏰</div>
-            <div className="middle__text-block">
-              <p>Call doctor for tests</p>
-              <p>9:00 AM</p>
-            </div>
-          </li>
-          <li className="middle__item">
-            <div className="middle__icon">⏰</div>
-            <div className="middle__text-block">
-              <p>Call doctor for tests</p>
-              <p>9:00 AM</p>
-            </div>
-          </li>
-          <li className="middle__item">
-            <div className="middle__icon">⏰</div>
-            <div className="middle__text-block">
-              <p>Call doctor for tests</p>
-              <p>9:00 AM</p>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </div>
-  );
+            <li className="middle__item">
+              <div className="middle__text-block">
+                <p>Call doctor for tests</p>
+                <p>9:00 AM</p>
+              </div>
+            </li>
+            <li className="middle__item">
+              <div className="middle__text-block">
+                <p>Call doctor for tests</p>
+                <p>9:00 AM</p>
+              </div>
+            </li>
+            <li className="middle__item">
+              <div className="middle__text-block">
+                <p>Call doctor for tests</p>
+                <p>9:00 AM</p>
+              </div>
+            </li>
+            </ul>
+        </div>
+
+       </div>
+
+    )
 }
+
 
 export default CenterMenu;
