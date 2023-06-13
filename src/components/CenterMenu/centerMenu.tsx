f3e1f5ea00e7a803c9fd9c0352f84ece3e5df61b
Elizaveta Dutova <elizaveta.dutova@dataart.com>
2023-06-13 13:15:29 +0200

    bug fix

--- src/components/CenterMenu/centerMenu.tsx
+++ src/components/CenterMenu/centerMenu.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/button-has-type */
 import React, { useState } from 'react';
 import './centerMenu.css';
 import { Todo, Todos } from 'types/types';
@@ -10,19 +9,19 @@ import DateBlock from './Date/DateBlock';
 function CenterMenu() {
   const [active, setActive] = useState(true);
 
-  function handleForm() {
+  const handleForm = () => {
     setActive(!active);
-  }
+  };
 
   const [todo, setTodo] = useState({ text: '', time: '', timeofday: '' });
   const [todos, setTodos] = useState<Todos>([]);
 
-  function addTodo(newTodo: Todo) {
+  const addTodo = (newTodo: Todo) => {
     if (newTodo) {
       setTodos([...todos, newTodo]);
       setTodo({ text: '', time: '', timeofday: '' });
     }
-  }
+  };
 
   return (
     <div className="middle">
@@ -33,12 +32,12 @@ function CenterMenu() {
         </div>
 
         <div>
-          <button className="plus" onClick={handleForm}>
+          <button type="submit" className="plus" onClick={handleForm}>
             <img src={plus} alt="plus" />
           </button>
         </div>
       </div>
-      {active && <Form todo={todo} setTodo={setTodo} addTodo={() => addTodo} />}
+      {active && <Form todo={todo} setTodo={setTodo} addTodo={addTodo} />}
 
       <TodoList todos={todos} />
     </div>
