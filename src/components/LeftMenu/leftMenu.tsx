1712ad6d45bd5d752c3c3c77cf98d1d0bdb5cfb6
Elizaveta Dutova <elizaveta.dutova@dataart.com>
2024-01-12 14:46:42 +0100

    left menu changes

--- src/components/LeftMenu/leftMenu.tsx
+++ src/components/LeftMenu/leftMenu.tsx
@@ -20,17 +20,15 @@ function LeftMenu() {
           <p className="weekly__header_big">Weekly Pinned</p>
           <p className="weekly__header_small">View all</p>
         </div>
-        <div className="list-block list-block_weekly">
-          <ul className="list">
-            <li className="item">
-              <div className="item__icon">⏰</div>
-              <div className="item__text-block">
-                <p className="item__text_big">Something to do</p>
-                <p className="item__text_small">11:11 AM</p>
-              </div>
-            </li>
-          </ul>
-        </div>
+        <ul className="list">
+          <li className="item">
+            <div className="item__icon">⏰</div>
+            <div className="item__text-block">
+              <p className="item__text_big">Something to do</p>
+              <p className="item__text_small">11:11 AM</p>
+            </div>
+          </li>
+        </ul>
         <Calendar
           className="calendar"
           showFixedNumberOfWeeks={false}
